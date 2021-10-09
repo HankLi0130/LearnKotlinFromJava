2801582e0a8448c00beb440326745b9c18ddba76
Hank Li <HankLi0130@gmail.com>
2021-10-09 09:35:24 +0800

    LKFJ04 completed

--- src/main/java/app/hankdev/lkfj04/LKFJ04.kt
+++ src/main/java/app/hankdev/lkfj04/LKFJ04.kt
@@ -4,4 +4,7 @@ fun main() {
     val person = Person("Hank", true)
     println(person.name)
     println(person.isMarried)
+
+    val rectangle = Rectangle(10, 10)
+    println(rectangle.isSquare)
 }
